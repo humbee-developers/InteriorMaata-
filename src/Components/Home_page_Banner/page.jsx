aa7f8640d2846c6e881c40e547995fb43ccf7bb4
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-04-24 17:54:22 +0530

    new push 8

--- src/Components/Home_page_Banner/page.jsx
+++ src/Components/Home_page_Banner/page.jsx
@@ -442,6 +442,7 @@ const Animation = ({ loadImage, counter }) => {
       <path class="nectar-scroll-icon-path" fill="none" stroke="#ffffff" stroke-width="2" stroke-miterlimit="10" d="M15,1.118c12.352,0,13.967,12.88,13.967,12.88v18.76  c0,0-1.514,11.204-13.967,11.204S0.931,32.966,0.931,32.966V14.05C0.931,14.05,2.648,1.118,15,1.118z">
       </path>
     </svg>
+    <div className="scroll">Scroll to explore</div>
      </div>}
      </div>
 
