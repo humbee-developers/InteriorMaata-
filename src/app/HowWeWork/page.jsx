0860316119c4d8269bc38452c18fbca0e24159bd
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-05-21 16:37:01 +0530

    new push 2

--- src/app/HowWeWork/page.jsx
+++ src/app/HowWeWork/page.jsx
@@ -4,11 +4,13 @@ import Stairs from "@/Animations/Stairs";
 import HowWeWorkHeader from "@/Components/HowWeWorkHeader/page";
 import HowWeWorkTimeline from "@/Components/HowWeWorkTimeline/page";
 import HowWeWorkText from "@/Components/HowWeWorkText/page";
+import TextRevel from "@/Animations/TextRevel/page";
 import ServicesCards from "@/Components/ServicesCards/page";
 // import ScrollText from "@/Components/ScrollText/page";
 import Services_Slider from "@/Components/Service_Slider/page";
 import AboutUs_ourDesign from "@/Components/AboutUs_ourDesign/page";
 const Page = () => {
+  const phrase = 'Get Your Home Designed In The Comfort Of Your Home Without Any Hassle. Our Team Of Excellent Designers Will Guide You In Designing Your Home Exactly The Way You Want.';
   useEffect(() => {
     (async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
@@ -20,6 +22,7 @@ const Page = () => {
       <Stairs/>
         <HowWeWorkHeader />
         <HowWeWorkTimeline />
+        <TextRevel phrase={phrase} />
         {/* <ScrollText/> */}
          <AboutUs_ourDesign />
         <ServicesCards />
