89de221a2f0e3b9e42e21bb49a90eebe723e2ddf
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-05-23 12:01:16 +0530

    new change

--- src/app/Services/page.jsx
+++ src/app/Services/page.jsx
@@ -10,35 +10,42 @@ import AboutUs_ourDesign from "@/Components/AboutUs_ourDesign/page";
 import Lenis from "@studio-freight/lenis";
 const Page = () => {
   const phrase = 'Get Your Home Designed In The Comfort Of Your Home Without Any Hassle. Our Team Of Excellent Designers Will Guide You In Designing Your Home Exactly The Way You Want.';
-  const lenisRef = useRef(null);
+  // const lenisRef = useRef(null);
+
+  // useEffect(() => {
+  //   // Initialize Lenis with options similar to Locomotive Scroll
+  //   const lenis = new Lenis({
+  //     duration: 1.2, // Duration of the scroll animation
+  //     easing: (t) => 1 - Math.pow(1 - t, 3), 
+  //     // easing: (t) => Math.min(1 - Math.pow(2, -10 * t)), // Easing function for a smooth scroll
+  //     smooth: true,
+  //     direction: "vertical",
+  //     gestureDirection: "vertical",
+  //     smoothTouch: true,
+  //     touchMultiplier: 2, // Adjust the touch sensitivity
+  //   });
+
+  //   // Function to continuously update Lenis
+  //   function raf(time) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
+
+  //   requestAnimationFrame(raf);
+
+  //   lenisRef.current = lenis;
+
+  //   return () => {
+  //     lenis.destroy();
+  //   };
+  // }, []);
 
   useEffect(() => {
-    // Initialize Lenis with options similar to Locomotive Scroll
-    const lenis = new Lenis({
-      duration: 1.2, // Duration of the scroll animation
-      easing: (t) => 1 - Math.pow(1 - t, 3), 
-      // easing: (t) => Math.min(1 - Math.pow(2, -10 * t)), // Easing function for a smooth scroll
-      smooth: true,
-      direction: "vertical",
-      gestureDirection: "vertical",
-      smoothTouch: true,
-      touchMultiplier: 2, // Adjust the touch sensitivity
-    });
-
-    // Function to continuously update Lenis
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    lenisRef.current = lenis;
-
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
 
   return (
     <>
--- src/app/Single_Project_Layout/page.jsx
+++ src/app/Single_Project_Layout/page.jsx
@@ -19,6 +19,12 @@ import Interior_last_room_Svg from "@/svgs/Interior_Lastroom.svg";
 import Lenis from "@studio-freight/lenis";
 import styles from "@/app/Single_Project_Layout/Single_project.module.css";
 const Project_Header = () => {
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
   const [ref, inView] = useInView({
     triggerOnce: true,
   });
@@ -73,35 +79,35 @@ const Project_Header = () => {
 
 
 
-  const lenisRef = useRef(null);
+  // const lenisRef = useRef(null);
 
-  useEffect(() => {
-    // Initialize Lenis with options similar to Locomotive Scroll
-    const lenis = new Lenis({
-      duration: 1.2, // Duration of the scroll animation
-      easing: (t) => 1 - Math.pow(1 - t, 3), 
-      // easing: (t) => Math.min(1 - Math.pow(2, -10 * t)), // Easing function for a smooth scroll
-      smooth: true,
-      direction: "vertical",
-      gestureDirection: "vertical",
-      smoothTouch: true,
-      touchMultiplier: 2, // Adjust the touch sensitivity
-    });
+  // useEffect(() => {
+  //   // Initialize Lenis with options similar to Locomotive Scroll
+  //   const lenis = new Lenis({
+  //     duration: 1.2, // Duration of the scroll animation
+  //     easing: (t) => 1 - Math.pow(1 - t, 3), 
+  //     // easing: (t) => Math.min(1 - Math.pow(2, -10 * t)), // Easing function for a smooth scroll
+  //     smooth: true,
+  //     direction: "vertical",
+  //     gestureDirection: "vertical",
+  //     smoothTouch: true,
+  //     touchMultiplier: 2, // Adjust the touch sensitivity
+  //   });
 
-    // Function to continuously update Lenis
-    function raf(time) {
-      lenis.raf(time);
-      requestAnimationFrame(raf);
-    }
+  //   // Function to continuously update Lenis
+  //   function raf(time) {
+  //     lenis.raf(time);
+  //     requestAnimationFrame(raf);
+  //   }
 
-    requestAnimationFrame(raf);
+  //   requestAnimationFrame(raf);
 
-    lenisRef.current = lenis;
+  //   lenisRef.current = lenis;
 
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+  //   return () => {
+  //     lenis.destroy();
+  //   };
+  // }, []);
 
   return (
     <Stairs>
