dbd9d36a3baa62a05d67f62b9205d5ba0816b236
Jay Kalariya <93344658+Jay-Kalariya@users.noreply.github.com>
2024-05-28 17:57:22 +0530

    klkkll

---
+++ src/Components/Social_media/page.jsx
@@ -0,0 +1,16 @@
+
+import React, { useEffect } from "react";
+// import React, { useRef } from 'https://cdn.skypack.dev/react';
+// import ReactDOM from 'https://cdn.skypack.dev/react-dom';
+// import { Scrollable } from 'https://cdn.skypack.dev/webrix/components';
+// import { ResizeObserver } from 'https://cdn.skypack.dev/webrix/tools';
+// import PropTypes from 'https://cdn.skypack.dev/prop-types';
+
+const SocialMedia = () => {
+     return (
+    <>
+    
+    </>
+  );
+};
+export default SocialMedia;
--- src/app/SocialMedia/page.jsx
+++ src/app/SocialMedia/page.jsx
@@ -1,18 +1,19 @@
-"use client"
-import React,  { useEffect }  from 'react'
-
+"use client";
+import React, { useEffect, useState, useRef } from "react";
+import SocialMedia from "@/Components/Social_media/page";
 const page = () => {
-    useEffect(() => {
-        (async () => {
-          const LocomotiveScroll = (await import("locomotive-scroll")).default;
-          const locomotiveScroll = new LocomotiveScroll();
-        })();
-      }, []);
+  
+  useEffect(() => {
+    (async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      const locomotiveScroll = new LocomotiveScroll();
+    })();
+  }, []);
   return (
     <>
-      
+      <SocialMedia />
     </>
-  )
-}
+  );
+};
 
-export default page
+export default page;
