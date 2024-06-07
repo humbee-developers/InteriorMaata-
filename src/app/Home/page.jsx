b90f1875e3b262b6866d018cde9f9db55e0d5661
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-06-07 14:13:27 +0530

    new push

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,8 +1,9 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
+import dynamic from 'next/dynamic';
 import Preloader from "@/Animations/preloader/index";
 import { AnimatePresence } from "framer-motion";
-import HomeBanner from "@/Components/Home_page_Banner/page";
+// import HomeBanner from "@/Components/Home_page_Banner/page";
 import AboutUs_header from "@/Components/AboutUs_header/page";
 import AboutUs_threeCards from "@/Components/AboutUs_threeCards/page";
 import AboutUs_Carousel2 from "@/Components/AboutUs_Carousel2/page";
@@ -11,44 +12,41 @@ import MarqueeTest from "@/Components/MarqueeTest/page"
 import AboutUsInfo from "@/Components/AboutUsInfo/page";
 import NewRevel from "@/Animations/newRevel/page";
 import "./home.css"
+
+
+
+
+const HomeBanner = dynamic(() => import("@/Components/Home_page_Banner/page"));
+const HomeBanner1 = dynamic(() => import("@/Components/Home_page_Banner/page2"));
+
+
+
 const Page = ({ lData }) => {
   const paragraph = 'INTERIORMAATA STUDIO : STEP INTO A REALM WHERE TRADITION DANCE WITH MODERN MINIMALISM, CREATING SPACES THAT RESONATE WITH SOULFUL ELEGANCE';
   const [isLoading, setIsLoading] = useState(true);
-  const [isCounter,setCounter]= useState  (0)
+  const [isCounter,setCounter]= useState  (0);
+  const [width, setWidth] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+    };
+
+    handleResize(); // Initialize width
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+
   useEffect(() => {
     (async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
       const locomotiveScroll = new LocomotiveScroll();
     })();
   }, []);
-  // const lenisRef = useRef(null);
-  // useEffect(() => {
-  //   // Initialize Lenis with options similar to Locomotive Scroll
-  //   const lenis = new Lenis({
-  //     duration: 1.2, // Duration of the scroll animation
-  //     easing: (t) => 1 - Math.pow(1 - t, 3), 
-  //     // easing: (t) => Math.min(1 - Math.pow(2, -10 * t)), // Easing function for a smooth scroll
-  //     smooth: true,
-  //     direction: "vertical",
-  //     gestureDirection: "vertical",
-  //     smoothTouch: true,
-  //     touchMultiplier: 2,  // Adjust the touch sensitivity
-  //   });
-
-  //   // Function to continuously update Lenis
-  //   function raf(time) {
-  //     lenis.raf(time);
-  //     requestAnimationFrame(raf);
-  //   }
-
-  //   requestAnimationFrame(raf);
-
-  //   lenisRef.current = lenis;
-
-  //   return () => {
-  //     lenis.destroy();
-  //   };
-  // }, []);
   
   function handleLoad(data) {
     console.log("data", data);
@@ -65,7 +63,16 @@ const Page = ({ lData }) => {
       <AnimatePresence mode="wait">
         {isLoading && <Preloader counter={isCounter} />}
       </AnimatePresence>
-      <HomeBanner loadImage={handleLoad} counter={handleCounter} />
+      {width &&(
+        <>
+          {width > 575 ?(
+            <HomeBanner loadImage={handleLoad} counter={handleCounter} />
+          ):(
+            <HomeBanner1 loadImage={handleLoad} counter={handleCounter} />
+          )}
+        </>
+      )}
+      
     
         <div className="bg">
         <AboutUs_header />
