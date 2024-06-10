3a86b03d9b53416bd85179bf26f71e37cb24b3e8
utkarshyadav11 <utkarshyadav6745@gmail.com>
2024-06-10 19:23:25 +0530

    dbie

--- src/app/KnowAboutUs/page.jsx
+++ src/app/KnowAboutUs/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
 import Stairs from "@/Animations/Stairs";
-import AboutusAnanya from "@/Components/AboutusAnanya/page"
+// import AboutusAnanya from "@/Components/AboutusAnanya/page"
 import TeamsPage from "@/Components/TPage/page";
 const Page = () => {
   useEffect(() => {
@@ -13,7 +13,7 @@ const Page = () => {
   return (
     <>
       <Stairs/>
-      <AboutusAnanya/>
+      {/* <AboutusAnanya/> */}
         <TeamsPage />
     </>
   );
